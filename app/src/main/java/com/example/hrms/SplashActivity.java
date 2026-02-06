ab879e41ab946e93fb065577521078bbb3e060a0
Apurva <apurvabharoliya7@gmail.com>
2026-02-06 15:47:00 +0530

    small changes

--- app/src/main/java/com/example/hrms/SplashActivity.java
+++ app/src/main/java/com/example/hrms/SplashActivity.java
@@ -3,11 +3,16 @@ package com.example.hrms;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hrms.R;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,6 +20,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView imgLogo = findViewById(R.id.imgLogo);
+
+        // Fade-in animation
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setDuration(1000);
+        imgLogo.startAnimation(fadeIn);
 
         new Handler().postDelayed(() -> {
             if (FirebaseAuth.getInstance().getCurrentUser() != null) {
